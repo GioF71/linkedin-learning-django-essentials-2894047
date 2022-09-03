8a1b1b3502160616a88d512e13043ba6cb62689a
GioF71 <giovanni.fulco@gmail.com>
2022-09-03 12:28:17 +0200

    secret regenerate and not stored

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*t7)v2#g&5%8n*4*)5!judqp!kmys$su@&kztl&$ibq5wnr#8_'
+with open('/tutorial/secret-key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
